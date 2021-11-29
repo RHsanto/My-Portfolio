a753ae43931fcc2be221ebe55b37a0118a47321a
Rakibul santo <SANTODON176@gmail.com>
2021-11-29 23:32:02 +0600

    add

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import './Contact.css'
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='contact-section p-5'>
+    <div className="container">
+    <h1>Contact Me</h1>
+      <div className="row">
+        <div className="col-lg-6">
+          <div className="div-1">
+            <h2>Phone</h2>
+            <p>+08801887403752</p>
+          </div>
+          <div className="div-2">
+            <h2>Email</h2>
+            <p>rakibul.hasan888@gmail.com</p>
+          </div>
+          <div className="div-3">
+            <h2>Location</h2>
+            <p>Narayongonj, Bangladesh</p>
+          </div>
+          <div className="div-4"></div>
+        </div>
+        <div className="col-lg-6">
+
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
